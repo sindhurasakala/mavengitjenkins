c26bb768873e9962af64771a6d2f85240acbfbc0
sindhurasakala <sakalasindhu@gmail.com>
2020-05-18 22:54:20 +0530

    added all project files

---
+++ src/main/java/sample2/Class1.java
@@ -0,0 +1,9 @@
+package sample2;
+
+public class Class1 {
+	public String sample()
+	{
+		return "sample";
+	}
+
+}
---
+++ src/test/java/sample2/Class1_test.java
@@ -0,0 +1,14 @@
+package sample2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Class1_test {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
